00749723eb184995b17f99efdcfada1c62fe2694
yaongfeng <yaongfeng@gmail.com>
2017-05-03 23:22:35 +0800

    initialize

---
+++ QMUIDemo_Swift/QMUIDemo_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QMUIDemo_Swift
+//
+//  Created by yaongfeng on 2017/5/3.
+//  Copyright © 2017年 yaongfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
